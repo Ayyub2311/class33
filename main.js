25c77abc6f6574a243a5da5826d7b6b12a8593cc
Ayyub2311 <157877827+Ayyub2311@users.noreply.github.com>
2024-04-26 23:35:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+$toggleClassNew('.btn-1', '.section-1', 'active')
+$toggleClassNew('.section-1', '.section-1', 'border')
+$toggleClassNew('.btn-2', '.section-2', 'active')
+$toggleClassNew('.btn-3', '.section-3', 'active')
+$sliceWordPerSecond4('.section-2','h2')
+
+
